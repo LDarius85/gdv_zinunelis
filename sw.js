a048c72684c6cb437b6016e678a3799dd76d96b8
LDarius85 <liekmanisdarius@gmail.com>
2025-04-22 19:59:18 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
-  self.skipWaiting();
+  //self.skipWaiting();
 });
 
 // Aktyvuojant – ištrinti senus cache'us
